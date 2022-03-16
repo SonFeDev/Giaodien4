e3836d228c762f8618d8e6637e06883ac2adc119
Son aka <sonliver281@gmail.com>
2022-03-16 12:14:50 +0700

    Update gd 4

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    function activeTab(obj) {
+        $('.tab ul li').removeClass('active');
+
+        $(obj).addClass('active');
+
+        var id = $(obj).find('a').attr('href');
+
+        $('.tab-item').hide();
+
+        $(id).show();
+    }
+
+    $('.tab li').click(function () {
+        activeTab(this);
+        return false;
+    });
+
+    activeTab($('.tab li:first-child'));
+});
